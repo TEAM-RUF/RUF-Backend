1c1b6c7b0b692d079996d869c37d5026c1768088
민상연 <judemin2087@naver.com>
2023-06-13 02:15:56 +0000

    [UPDATE] updated survey mongoose func

--- models/survey_data.js
+++ models/survey_data.js
@@ -28,11 +28,28 @@ const surveySchema = mongoose.Schema({
 });
 
 surveySchema.statics.getIdByToken  = async (token) => {
-  	const _id = await UserData.getIdByToken(token);
-	// promise 형태로 넘어오는 _id를 처리 이후 find
-	const user = await UserData.findById(new ObjectID(_id));
-	// await를 사용할 때 에러가 발생하는 경우가 있음 (ide 문제?)
-	return user.surveyID;
+	try{
+		const _id = await UserData.getIdByToken(token);
+		// promise 형태로 넘어오는 _id를 처리 이후 find
+		const user = await UserData.findById(new ObjectID(_id));
+		// await를 사용할 때 에러가 발생하는 경우가 있음 (ide 문제?)
+		return user.surveyID;
+	}catch(err){
+		return null;
+	}
+};
+
+surveySchema.statics.saveSurveyID  = async (_surveyID, token) => {
+	try{
+		const _id = await UserData.getIdByToken(token);
+		const user = await UserData.findOneAndUpdate(
+				{ _id: new ObjectID(_id) },
+				{ surveyID: _surveyID }
+		);
+	}catch(err){
+		console.log(err);
+		return err;
+	}
 };
 
 const SurveyData = mongoose.model("SurveyData", surveySchema);
--- routes/survey.js
+++ routes/survey.js
@@ -10,8 +10,9 @@ router.get("/", (req, res) => {
 
 router.post('/addSurvey', async (req, res) => {
 	try {
+		const token = req.body.token;
 		const surveyData = new SurveyData(req.body);
-		const surveyID = await SurveyData.getIdByToken(req.body.token);
+		const surveyID = await SurveyData.getIdByToken(token);
 		
 		if(!surveyID){
 			// 해당 유저 없음
@@ -19,15 +20,18 @@ router.post('/addSurvey', async (req, res) => {
 		}else if(surveyID == "N/A"){
 			// 설문조사 추가
 			const surveyAddStatus = await surveyData.save();
+			await SurveyData.saveSurveyID(surveyAddStatus._id, token);
+			
 			if (!surveyAddStatus){
 				const err = new Error("Internal Server Error");
 				return res.status(500).json({success: false, err});
 			}
+			
 			console.log(surveyAddStatus);
 			return res.status(200).json({success: true});
 		}else{
-			// 설문조사 업데이트
-			return res.status(400).json({success: false});
+			// 설문조사가 존재하는 상태에서 요청이 들어왔을 때
+			return res.status(400).json({success: false, err : "Survey already exists"});
 		}
 	}catch (err) {
 		res.status(500).send(err);
