3db8f9b31e8bac1bb3c8ae1b93cc17f4bd832893
민상연 <judemin2087@naver.com>
2023-08-23 09:02:42 +0000

    [FIX] fixed white space

--- routes/video.js
+++ routes/video.js
@@ -10,54 +10,52 @@ var router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-	router.post('/upload', upload.single('file'), async (req, res) => {
-		try {
-			if (!req.file) {
-				throw new Error('No file uploaded.');
-			}
-			
-			const file = req.file;
-			
-			const conn = mongoose.connection;
-			const bucket = new mongoose.mongo.GridFSBucket(conn.db, {
-				bucketName: 'uploads'
-			});
-
-			const uploadStream = bucket.openUploadStream(file.originalname, {
-				contentType: file.mimetype
-			});
-
-			console.log("1");
-			const readableStream = new Readable();
-			readableStream.push(file.buffer);
-			readableStream.push(null);
-			
-			readableStream.pipe(uploadStream);
-			
-			uploadStream.on('finish', async (uploadedFile) => {
-				const video = new VideoModel({
-					title: req.body.title,
-					description: req.body.description,
-					filename: uploadedFile.filename,
-					contentType: uploadedFile.contentType,
-					userToken: req.body.userToken,
-				});
-				
-				await video.save();
-				
-				return res.status(200).json({
-					success: true,
-					message: 'File upload success',
-				});
-			});
-		} catch (err) {
-			console.error(err);
-			return res.status(500).json({
-				success: false,
-				message: err.message,
-			});
+router.post('/upload', upload.single('file'), async (req, res) => {
+	try {
+		if (!req.file) {
+			throw new Error('No file uploaded.');
 		}
-	});
-	
+		
+		const file = req.file;
+		
+		const conn = mongoose.connection;
+		const bucket = new mongoose.mongo.GridFSBucket(conn.db, {
+			bucketName: 'uploads'
+		});
+			const uploadStream = bucket.openUploadStream(file.originalname, {
+			contentType: file.mimetype
+		});
+		
+		const readableStream = new Readable();
+		readableStream.push(file.buffer);
+		readableStream.push(null);
+		
+		readableStream.pipe(uploadStream);
+		
+		uploadStream.on('finish', async (uploadedFile) => {
+			const video = new VideoModel({
+				title: req.body.title,
+				description: req.body.description,
+				filename: uploadedFile.filename,
+				contentType: uploadedFile.contentType,
+				userToken: req.body.userToken,
+			});
+			
+			await video.save();
+			
+			return res.status(200).json({
+				success: true,
+				message: 'File upload success',
+			});
+		});
+	} catch (err) {
+		console.error(err);
+		return res.status(500).json({
+			success: false,
+			message: err.message,
+		});
+	}
+});
+
 module.exports = router;
 	
