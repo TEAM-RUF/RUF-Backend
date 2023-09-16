607cf4ca355fcb10eb7a63410e9deb1c389a42e2
judemin <judemin2087@naver.com>
2023-09-16 22:04:19 +0900

    [ADD] add python-shell into image_proc router

--- routes/image_proc.js
+++ routes/image_proc.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const { PythonShell } = require("python-shell");
 
 router.get("/", (req, res) => {
-    res.status(200).send({ message: "영상처리 로직 라우터" });
+    const filePath = path.join(__dirname, '/image_processing/squatRight.py');
+
+    const options = {
+        mode: 'text',
+        pythonPath: '',
+        pythonOptions: ['-u'],
+        scriptPath: '',
+        args: ['arg1', 'arg2'],
+    };
+
+    PythonShell.run(filePath, options, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('An error occurred while running Python script.');
+        } else {
+            console.log('Python script executed successfully.');
+            res.send(result);
+        }
+    });
 });
 
 module.exports = router;
