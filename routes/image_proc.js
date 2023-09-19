595171bf781f6b1c42ce64aa89cf3bca300ee791
judemin <judemin2087@naver.com>
2023-09-19 16:57:16 +0900

    [ADD] add bench right processing logic

--- routes/image_proc.js
+++ routes/image_proc.js
@@ -26,4 +26,34 @@ router.get("/", (req, res) => {
     });
 });
 
+app.post("/upload", upload.single("image"), (req, res) => {
+    const imageBuffer = req.file.buffer
+    let count = req.body.count;
+
+    const filePath = path.join(__dirname, '/image_processing/benchRightProc.py');
+    const options = {
+        mode: 'text',
+        pythonPath: '',
+        pythonOptions: ['-u'],
+        scriptPath: '',
+        args: [imageBuffer.toString("base64"), count.toString()]
+    };
+
+    PythonShell.run(filePath, options, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('An error occurred while running Python script.');
+        } else {
+            console.log('Python script executed successfully.');
+            const pythonResponse = JSON.parse(result);
+
+            resCount = pythonResponse.res_count;
+
+            const processedImageBase64 = pythonResponse.processed_image;
+
+            res.json({ resCount, processedImageBase64 });
+        }
+    });
+});
+
 module.exports = router;
